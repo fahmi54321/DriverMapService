45ad77a73cc738b574d0ecafae2d1db5fbf0fef6
fahmi54321 <fahmiabdulaziz44@gmail.com>
2021-04-28 21:43:17 +0700

    Map

--- app/src/main/java/com/android/drivermapservice/MapsActivity.kt
+++ app/src/main/java/com/android/drivermapservice/MapsActivity.kt
@@ -2,10 +2,36 @@ package com.android.drivermapservice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MapsActivity : AppCompatActivity() {
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    //todo 1 map
+    private lateinit var mMap: GoogleMap
+    private lateinit var mapFragment:SupportMapFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
+
+        //todo 2 map
+        mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    //todo 3 map (finish)
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 }
